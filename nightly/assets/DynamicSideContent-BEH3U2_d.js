13b93860612418501ae8518d36e7963985772f5d
service-tip-git <tmsatsls+github.com_service-tip-git@sap.com>
2026-04-15 05:38:23 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents-react@411967aa1fc335f27d17d3b4e39635adc6329ccc 🚀

---
+++ nightly/assets/DynamicSideContent-BEH3U2_d.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{Jm as t}from"./iframe-nfBer4sh.js";import{r as n}from"./react-Cw_hWkS_.js";import{_ as r,o as i,p as a,s as o,t as s}from"./components-SwiI1EA_.js";import{_ as c,d as l,r as u}from"./blocks-CFRyz5Z7.js";import{Default as d,n as f,t as p}from"./DynamicSideContent.stories-DTOCVw6I.js";function m(e){let t={h2:`h2`,...n(),...e.components};return(0,g.jsxs)(g.Fragment,{children:[(0,g.jsx)(l,{of:p}),`
+`,(0,g.jsx)(o,{of:p,since:`0.21.0`}),`
+`,(0,g.jsx)(`br`,{}),`
+`,(0,g.jsx)(t.h2,{id:`example`,children:`Example`}),`
+`,(0,g.jsx)(u,{of:d}),`
+`,(0,g.jsx)(t.h2,{id:`properties`,children:`Properties`}),`
+`,(0,g.jsx)(a,{of:d}),`
+`,(0,g.jsx)(i,{})]})}function h(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,g.jsx)(t,{...e,children:(0,g.jsx)(m,{...e})}):m(e)}var g;e((()=>{g=t(),r(),s(),c(),f()}))();export{h as default};
