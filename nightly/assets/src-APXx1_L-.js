45f2d25a0f9d9d1034624a6611fe01df8231f2e5
service-tip-git <tmsatsls+github.com_service-tip-git@sap.com>
2026-06-03 05:37:07 +0000

    Deploying to gh-pages from @ service-tip-git/ui5-webcomponents-react@c7d72c9b0887fc0d3bae89ee0b6462523cc3ed13 🚀

---
+++ nightly/assets/src-APXx1_L-.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./LineChart-DVkG0Enz.js";var n,r=e((()=>{n={version:`2.22.2`,major:2,minor:22,patch:2,suffix:``,isNext:!1,buildTime:1780464971},n.version})),i=e((()=>{t(),r(),n.version}));export{i as t};
